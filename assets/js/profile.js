b1edbf1be8d1011a78f3af6f8b2e68f17540c3f2
joaquin@oktana.io <joaquingimenez83@gmail.com>
2019-04-24 14:33:42 -0300

    profile advances

--- assets/js/profile.js
+++ assets/js/profile.js
@@ -7,21 +7,25 @@ $(document).ready(function() {
 
 function handleEdit() 
 {
-  var inputs = $(".profile-about input");
+  var inputs = $(".profile-about input[type=text]");
+  var button = $(".profile-about input[type=submit]");
   $.each(inputs, function(key, input) {
     $(input).prop('readonly', false);
     $(input).removeClass("readonly");
   });
+  $(button).removeClass('display-none');
   $("#editprofile").addClass("display-none");
   $("#cancelprofile").removeClass("display-none");
 }
 
 function cancelEdit() {
-  var inputs = $(".profile-about input");
+  var inputs = $(".profile-about input[type=text]");
+  var button = $(".profile-about input[type=submit]");
   $.each(inputs, function(key, input) {
     $(input).prop('readonly', true);
     $(input).addClass("readonly");
   });
+  $(button).addClass('display-none');
   $("#editprofile").removeClass("display-none");
   $("#cancelprofile").addClass("display-none");
 }
